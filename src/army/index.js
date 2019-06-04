4d16451125d508573b52b6191e7a086cc977fb13
Bui Vuong <vuongbui@vnnginfotech.com>
2019-06-04 10:35:23 +0700

    ss

--- src/army/index.js
+++ src/army/index.js
@@ -1,15 +1,4 @@
 import titan_universe from "../containers/army/titan_universe";
-import sahl_dynasty from "../containers/army/sahl_dynasty";
-import elemental_universe from "../containers/army/elemental_universe";
-import imkan_heaven from "../containers/army/imkan_heaven";
-import yiddenda_realm from "../containers/army/yiddenda_realm";
-import bhamka_crypt from "../containers/army/bhamka_crypt";
-import vargan_clan from "../containers/army/vargan_clan";
-import roshri_shadow from "../containers/army/roshri_shadow";
-import cobalt_guild from "../containers/army/cobalt_guild";
-import elyrth_shadow from "../containers/army/elyrth_shadow";
-import dular_tribe from "../containers/army/dular_tribe";
-import idu_clan from "../containers/army/idu_clan";
 import krolaer_crypt from "../containers/army/krolaer_crypt";
 import paladin_armada from "../containers/army/paladin_armada";
 import felose_arcane from "../containers/army/felose_arcane";
@@ -20,7 +9,6 @@ import drioce_realm from "../containers/army/drioce_realm";
 import uvniah_shadow from "../containers/army/uvniah_shadow";
 import xuelong_heaven from "../containers/army/xuelong_heaven";
 import zhihao_tiantang from "../containers/army/zhihao_tiantang";
-import haahmiah_heaven from "../containers/army/haahmiah_heaven";
 import kusari_guild from "../containers/army/kusari_guild";
 import outlandish_land from "../containers/army/outlandish_land";
 import xamin_crypt from "../containers/army/xamin_crypt";
@@ -36,17 +24,12 @@ import perigors_church from "../containers/army/perigors_church";
 import nebula_system from "../containers/army/nebula_system";
 import olympus_summit from "../containers/army/olympus_summit";
 import eilif_norse from "../containers/army/eilif_norse";
-import morte_crypt from "../containers/army/morte_crypt";
 import richolda_moon from "../containers/army/richolda_moon";
 import gereminus_order from "../containers/army/gereminus_order";
-import xazis_crypt from "../containers/army/xazis_crypt";
-import gnung_tribe from "../containers/army/gnung_tribe";
 import burntongue_hell from "../containers/army/burntongue_hell";
 import dagiel_imperial from "../containers/army/dagiel_imperial";
 import xothkug_tribe from "../containers/army/xothkug_tribe";
-import thallilgiot_realm from "../containers/army/thallilgiot_realm";
 import zadimus_crypt from "../containers/army/zadimus_crypt";
-import legic_guild from "../containers/army/legic_guild";
 import qhimmuls_planet from "../containers/army/qhimmuls_planet";
 import vagan_tribe from "../containers/army/vagan_tribe";
 import bradhos_crypt from "../containers/army/bradhos_crypt";
@@ -54,17 +37,6 @@ import joleth_sky from "../containers/army/joleth_sky";
 
 const army = {
     titan_universe,
-    sahl_dynasty,
-    elemental_universe,
-    imkan_heaven,
-    yiddenda_realm,
-    bhamka_crypt,
-    vargan_clan,
-    roshri_shadow,
-    cobalt_guild,
-    elyrth_shadow,
-    dular_tribe,
-    idu_clan,
     krolaer_crypt,
     paladin_armada,
     felose_arcane,
@@ -75,7 +47,6 @@ const army = {
     uvniah_shadow,
     xuelong_heaven,
     zhihao_tiantang,
-    haahmiah_heaven,
     kusari_guild,
     outlandish_land,
     xamin_crypt,
@@ -91,17 +62,12 @@ const army = {
     nebula_system,
     olympus_summit,
     eilif_norse,
-    morte_crypt,
     richolda_moon,
     gereminus_order,
-    xazis_crypt,
-    gnung_tribe,
     burntongue_hell,
     dagiel_imperial,
     xothkug_tribe,
-    thallilgiot_realm,
     zadimus_crypt,
-    legic_guild,
     qhimmuls_planet,
     vagan_tribe,
     bradhos_crypt,
